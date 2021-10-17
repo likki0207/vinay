f6856b2c5ffb5edca1d445c3330b81f10e449c14
likki0207 <68856803+likki0207@users.noreply.github.com>
2021-10-17 21:28:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,89 +1,82 @@
-import pandas as pd
 import streamlit as st
-import altair as alt
-from PIL import Image
+import pandas as pd
+import base64
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import yfinance as yf
 
-######################
-# Page Title
-######################
+st.title('S&P 500 App')
 
-image = Image.open('dna-logo.jpg')
-
-st.image(image, use_column_width=True)
-
-st.write("""
-# DNA Nucleotide Count Web App
-This app counts the nucleotide composition of query DNA!
-***
+st.markdown("""
+This app retrieves the list of the **S&P 500** (from Wikipedia) and its corresponding **stock closing price** (year-to-date)!
+* **Python libraries:** base64, pandas, streamlit, numpy, matplotlib, seaborn
+* **Data source:** [Wikipedia](https://en.wikipedia.org/wiki/List_of_S%26P_500_companies).
 """)
 
+st.sidebar.header('User Input Features')
 
-######################
-# Input Text Box
-######################
+# Web scraping of S&P 500 data
+#
+@st.cache
+def load_data():
+    url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
+    html = pd.read_html(url, header = 0)
+    df = html[0]
+    return df
 
-#st.sidebar.header('Enter DNA sequence')
-st.header('Enter DNA sequence')
+df = load_data()
+sector = df.groupby('GICS Sector')
 
-sequence_input = ">DNA Query 2\nGAACACGTGGAGGCAAACAGGAAGGTGAAGAAGAACTTATCCTATCAGGACGGAAGGTCCTGTGCTCGGG\nATCTTCCAGACGTCGCGACTCTAAATTGCCCCCTCTGAGGTCAAGGAACACAAGATGGTTTTGGAAATGC\nTGAACCCGATACATTATAACATCACCAGCATCGTGCCTGAAGCCATGCCTGCTGCCACCATGCCAGTCCT"
+# Sidebar - Sector selection
+sorted_sector_unique = sorted( df['GICS Sector'].unique() )
+selected_sector = st.sidebar.multiselect('Sector', sorted_sector_unique, sorted_sector_unique)
 
-#sequence = st.sidebar.text_area("Sequence input", sequence_input, height=250)
-sequence = st.text_area("Sequence input", sequence_input, height=250)
-sequence = sequence.splitlines()
-sequence = sequence[1:] # Skips the sequence name (first line)
-sequence = ''.join(sequence) # Concatenates list to string
+# Filtering data
+df_selected_sector = df[ (df['GICS Sector'].isin(selected_sector)) ]
 
-st.write("""
-***
-""")
+st.header('Display Companies in Selected Sector')
+st.write('Data Dimension: ' + str(df_selected_sector.shape[0]) + ' rows and ' + str(df_selected_sector.shape[1]) + ' columns.')
+st.dataframe(df_selected_sector)
 
-## Prints the input DNA sequence
-st.header('INPUT (DNA Query)')
-sequence
+# Download S&P500 data
+# https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
+def filedownload(df):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
+    href = f'<a href="data:file/csv;base64,{b64}" download="SP500.csv">Download CSV File</a>'
+    return href
 
-## DNA nucleotide count
-st.header('OUTPUT (DNA Nucleotide Count)')
+st.markdown(filedownload(df_selected_sector), unsafe_allow_html=True)
 
-### 1. Print dictionary
-st.subheader('1. Print dictionary')
-def DNA_nucleotide_count(seq):
-  d = dict([
-            ('A',seq.count('A')),
-            ('T',seq.count('T')),
-            ('G',seq.count('G')),
-            ('C',seq.count('C'))
-            ])
-  return d
+# https://pypi.org/project/yfinance/
 
-X = DNA_nucleotide_count(sequence)
+data = yf.download(
+        tickers = list(df_selected_sector[:10].Symbol),
+        period = "ytd",
+        interval = "1d",
+        group_by = 'ticker',
+        auto_adjust = True,
+        prepost = True,
+        threads = True,
+        proxy = None
+    )
 
-#X_label = list(X)
-#X_values = list(X.values())
+# Plot Closing Price of Query Symbol
+def price_plot(symbol):
+  df = pd.DataFrame(data[symbol].Close)
+  df['Date'] = df.index
+  plt.fill_between(df.Date, df.Close, color='skyblue', alpha=0.3)
+  plt.plot(df.Date, df.Close, color='skyblue', alpha=0.8)
+  plt.xticks(rotation=90)
+  plt.title(symbol, fontweight='bold')
+  plt.xlabel('Date', fontweight='bold')
+  plt.ylabel('Closing Price', fontweight='bold')
+  return st.pyplot()
 
-X
+num_company = st.sidebar.slider('Number of Companies', 1, 5)
 
-### 2. Print text
-st.subheader('2. Print text')
-st.write('There are  ' + str(X['A']) + ' adenine (A)')
-st.write('There are  ' + str(X['T']) + ' thymine (T)')
-st.write('There are  ' + str(X['G']) + ' guanine (G)')
-st.write('There are  ' + str(X['C']) + ' cytosine (C)')
-
-### 3. Display DataFrame
-st.subheader('3. Display DataFrame')
-df = pd.DataFrame.from_dict(X, orient='index')
-df = df.rename({0: 'count'}, axis='columns')
-df.reset_index(inplace=True)
-df = df.rename(columns = {'index':'nucleotide'})
-st.write(df)
-
-### 4. Display Bar Chart using Altair
-st.subheader('4. Display Bar chart')
-p = alt.Chart(df).mark_bar().encode(
-    x='nucleotide',
-    y='count'
-)
-p = p.properties(
-    width=alt.Step(80)  # controls width of bar.
-)
-st.write(p)
+if st.button('Show Plots'):
+    st.header('Stock Closing Price')
+    for i in list(df_selected_sector.Symbol)[:num_company]:
+        price_plot(i)
